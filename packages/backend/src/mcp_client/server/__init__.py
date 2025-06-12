521309f0c7f018f5944962c5f5454344c24d547b
ChangHun Yoon <drskur@amazon.com>
2025-06-12 20:17:58 +0900

    fix: delete backend

--- packages/backend/src/mcp_client/server/__init__.py
+++
@@ -1,1 +0,0 @@
-# MCP examples package 
--- packages/backend/src/routers/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-API 라우터 패키지
-"""
-
-from src.routers.agent_router import router as chat_router
-from src.routers.mcp_tools_router import router as mcp_tools_router
-from src.routers.prompt_router import router as prompt_router
-from src.routers.model_router import router as model_router
